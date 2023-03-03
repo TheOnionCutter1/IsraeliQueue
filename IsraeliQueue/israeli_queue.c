eb643e6402d05dfaa8c076931361361f463a8155
Omer <omeralon53@gmail.com>
2023-03-03 17:18:28 +0200

    Create an initial structure for the queue
    Create a function that creates an emptry queue

--- IsraeliQueue/israeli_queue.c
+++ IsraeliQueue/israeli_queue.c
@@ -1,11 +1,33 @@
 #include "israeli_queue.h"
 #include <stdlib.h>
 
+typedef struct
+{
+    int value;
+    struct Node* next;
+    struct Node* prev;
+} Node;
+
+typedef struct
+{
+    int groupID;
+    Node* groupHead;
+    Node* groupTail;
+    struct GroupNode* nextGroup;
+} GroupNode;
+
 struct IsraeliQueue
 {
+    GroupNode* head;
+    GroupNode* tail;
 };
 
 IsraeliQueue* israeliQueue_create()
 {
-    return NULL;
+    IsraeliQueue* q = malloc(sizeof(IsraeliQueue));
+
+    q->head = NULL;
+    q->tail = NULL;
+
+    return q;
 }
