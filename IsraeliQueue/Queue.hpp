a93af7b3e4e8020d2db2b2387909d9b0954ca6da
Omer <omeralon53@gmail.com>
2023-03-04 19:45:37 +0200

    Create `Queue::enqueue`

--- IsraeliQueue/Queue.hpp
+++ IsraeliQueue/Queue.hpp
@@ -1,20 +1,47 @@
 #pragma once
 
+#include <cstddef>
+
 namespace israeli_queue
 {
 template <class T> class Node
 {
 public:
     T value;
-    Node* next;
-    Node* prev;
+    Node<T>* next;
+    Node<T>* prev;
+
+    Node(T value) : value(value), next(NULL), prev(NULL)
+    {
+    }
 };
 
 template <class T> class Queue
 {
 private:
+    Node<T>* head;
+    Node<T>* tail;
+
 public:
-    void enqueue(T value);
+    Queue() : head(NULL), tail(NULL)
+    {
+    }
+
+    void enqueue(T value)
+    {
+        if (!head)
+        {
+            head = new Node<T>(value);
+            tail = new Node<T>(value);
+        }
+        else
+        {
+            tail->next       = new Node<T>(value);
+            tail->next->prev = tail;
+            tail             = tail->next;
+        }
+    }
+
     T dequeue();
 };
 }; // namespace israeli_queue
