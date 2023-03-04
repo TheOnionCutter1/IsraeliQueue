d4e07c193f6b3dfb9f587b837581d7fc56867341
Omer <omeralon53@gmail.com>
2023-03-04 20:58:23 +0200

    Create a function that "allocates" a new group ID

--- IsraeliQueue/IsraeliQueue.hpp
+++ IsraeliQueue/IsraeliQueue.hpp
@@ -6,9 +6,33 @@ namespace israeli_queue
 {
 template <class T> class IsraeliQueue
 {
-    Queue<int> groupQueue;
-    std::unordered_map<int, Node<T>> groupedValues;
+private:
+    Queue<int> _groupQueue;
+    std::unordered_map<int, Queue<T>> _groupedValues;
+    int _idCounter = 1;
 
+    /**
+     * Returns an unused group id.
+     */
+    int _findNewID()
+    {
+        int id = _idCounter++;
+
+        // Check if there was an overflow in `_idCounter`.
+        if (id < 0)
+        {
+            _idCounter = 1;
+            while (_groupedValues.find(_idCounter) != _groupedValues.end())
+            {
+                _idCounter++;
+            }
+            id = _idCounter;
+        }
+
+        return id;
+    }
+
+public:
     void enqueue(T value, int group = -1);
     T dequeue();
 };
